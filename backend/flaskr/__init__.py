67a548cbebec49b97cd911b8f61eed9058a678d8
johannes <johannes.birk11@web.de>
2022-12-27 21:47:05 +0100

    Update todo

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -233,12 +233,6 @@ def create_app(test_config=None):
                         }
                     )
 
-    """
-    @TODO:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
-
     @app.errorhandler(404)
     def not_found(error):
         return (
